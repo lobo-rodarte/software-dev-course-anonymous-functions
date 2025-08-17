26cff4be908e34d4c6852d3d2f25a5747813f3a8
mrodarte <mlrfortune@gmail.com>
2025-08-17 14:20:09 -0500

    Completed the assignment

--- project-task.js
+++ project-task.js
@@ -38,8 +38,8 @@ const todos = [
   2. Use an anonymous function as the callback.
   3. Return only the tasks that are not completed.
   */
-  
-  
+ let incompleteTasks = todos.filter((neededTasks) => !neededTasks.completed); // created a variable that filtered the todos array and created another array with the incompleted tasks. Using the arrow function checks the objects and creates another array of incomplete tasks.
+   
   /*
   🔹 Task 2: Sort Tasks by Priority
   
@@ -48,7 +48,7 @@ const todos = [
   2. Use an anonymous function as the comparison function.
   3. Sort tasks in ascending order of priority (1 = highest).
   */
-  
+  todos.sort((a, b) => a.priority - b.priority); // sorted the todos array and used a and b for comparison. I used the arrow function call to compare the priority in the objects. For ascending order, b had to be subtracted from a to show the a values first.
   
   /*
   🔹 Task 3: Mark All Tasks as Completed
@@ -58,8 +58,11 @@ const todos = [
   2. Use an anonymous function to modify each object.
   3. Change the `completed` property to `true` for every task.
   */
-  
-  
+  let mappedList = todos.map(newStatus => {
+    return { ...newStatus, completed: true 
+    };
+  });
+    
   /*
   🔹 Task 4: Combine Filters
   
@@ -68,14 +71,14 @@ const todos = [
   2. Then, sort the filtered results by priority using `sort()`.
   3. Use method chaining to perform both steps together.
   */
-  
+  let finalList = todos.filter(task => !task.completed).sort((a, b) => a.priority - b.priority); // This one was tricky I needed to fully understand the method chaining filter().sort() to get it, I was trying to fill it all in with the variables I collected. I used the incompleteTasks but realized I can't use the variables to reuse the conditions. which is crazy. Anyway, renamed the variable and filtered the todos array using the conditions outlined above and directly sorted it using the .sort and the conditions outlined above from the previous vairable.
   
   // ============================================
   // 🧪 Console Test Your Work
   // ============================================
   
-  // console.log("Incomplete Tasks:", ...);
-  // console.log("Sorted by Priority:", ...);
-  // console.log("All Tasks Completed:", ...);
-  // console.log("Sorted Incomplete Tasks:", ...);
+  console.log("Incomplete Tasks:", (incompleteTasks));
+  console.log("Sorted by Priority:", (todos));
+  console.log("All Tasks Completed:", (mappedList));
+  console.log("Sorted Incomplete Tasks:", (finalList));
   
